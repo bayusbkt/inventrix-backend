078eaa66e604c763813e4dcd0e45df2dda6f41df
bayy <bayusubekti00@gmail.com>
2024-09-17 09:03:01 +0700

    initial

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import session from "express-session";
+import sequelizeStore from "connect-session-sequelize"
+import { connection, sequelize } from "./Config/Database.js";
+
+dotenv.config();
+const PORT = process.env.port;
+const app = express();
+
+const sessionStore = sequelizeStore(session.Store)
+const store = new sessionStore({
+  db: sequelize
+})
+
+app.use(express.json());
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  })
+);
+
+app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: true,
+      store: store,
+      cookie: {
+        secure: "auto",
+        maxAge: 1000 * 60 * 15
+      },
+    })
+  );
+
+connection();
+app.listen(PORT, () => {
+  console.log("Server up and running...");
+});
